650e99c7c252939adf1d808aae5ab0abf79a35c8
martioc <martioc6464@gmail.com>
2022-08-10 11:11:32 +0200

    3

---
+++ Ejercicio 3/main.js
@@ -0,0 +1,12 @@
+var mes = "enero";
+console.log(mes);
+console.log(typeof(mes));
+var año = 2022;
+console.log(año);
+console.log(typeof(año));
+let dia ="Lunes";
+console.log(dia);
+const hora="12:00";
+console.log(hora);
+
+
