c258b184036cf8648842cd79ae55b019859da742
martioc <martioc6464@gmail.com>
2022-08-11 13:26:18 +0200

    Dawit work pls

--- Ejercicio 8/main.js
+++ Ejercicio 8/main.js
@@ -1,65 +1,18 @@
-mes = Math.round(Math.random(1) * 13);
 
-switch (mes) {
-  case 1:
-    console.log(1);
-    break;
-  case 2:
-    console.log(2);
-    break;
-  case 3:
-    console.log(3);
-    break;
-  case 4:
-    console.log(4);
-    break;
-  case 5:
-    console.log(5);
-    break;
-  case 6:
-    console.log(6);
-    break;
-  case 7:
-    console.log(7);
-    break;
-  case 8:
-    console.log(8);
-    break;
-  case 9:
-    console.log(9);
-    break;
-  case 10:
-    console.log(10);
-    break;
-  case 11:
-    console.log(11);
-    break;
-  case 12:
-    console.log(12);
-    break;
-  default:
-    console.log("victor is gay");
-    break;
-}
+console.log("¿Es cierto que francolachupa?");
 
-Francolachupa = Math.round(Math.random(1) * 6);
+let Francolachupa = Math.round(Math.random());
 
 switch (Francolachupa) {
+  case 0:
+    console.log("Si la chupa");
+    break;
   case 1:
-    console.log("One");
-    break;
-  case 2:
-    console.log("Two");
-    break;
-  case 3:
-    console.log("Three");
-    break;
-  case 4:
-    console.log("Four");
-    break;
-  case 5:
-    console.log("Five");
-    default:
-    console.log("Francolachupa");
+    console.log("Por el culo");
     break;
 }
+
+const result = Math.PI.toFixed(3);
+console.log(result);
+
+console.log(Math.floor(Math.PI * 1000)/1000);
---
+++ Ejercicio 9/main.js
@@ -0,0 +1,65 @@
+mes = Math.round(Math.random(1) * 13);
+
+switch (mes) {
+  case 1:
+    console.log(1);
+    break;
+  case 2:
+    console.log(2);
+    break;
+  case 3:
+    console.log(3);
+    break;
+  case 4:
+    console.log(4);
+    break;
+  case 5:
+    console.log(5);
+    break;
+  case 6:
+    console.log(6);
+    break;
+  case 7:
+    console.log(7);
+    break;
+  case 8:
+    console.log(8);
+    break;
+  case 9:
+    console.log(9);
+    break;
+  case 10:
+    console.log(10);
+    break;
+  case 11:
+    console.log(11);
+    break;
+  case 12:
+    console.log(12);
+    break;
+  default:
+    console.log("victor is gay");
+    break;
+}
+
+Francolachupa = Math.round(Math.random(1) * 6);
+
+switch (Francolachupa) {
+  case 1:
+    console.log("One");
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  case 3:
+    console.log("Three");
+    break;
+  case 4:
+    console.log("Four");
+    break;
+  case 5:
+    console.log("Five");
+    default:
+    console.log("Francolachupa");
+    break;
+}
