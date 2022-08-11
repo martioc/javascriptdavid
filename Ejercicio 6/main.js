acfe873f0c3e80e04634f4df16c52a60c2538b2f
martioc <martioc6464@gmail.com>
2022-08-11 10:20:12 +0200

    dawit is gay

---
+++ Ejercicio 6/main.js
@@ -0,0 +1,24 @@
+function aguacate(){
+    return(1+1+1)
+};
+
+console.log(aguacate());
+
+
+
+function nombrecompleto(nombre,apellido1,apellido2){
+return nombre+" "+apellido1+" "+apellido2
+
+};
+
+console.log(nombrecompleto("Juan Carlos","Rey de","su puta madre"));
+
+let penes = [8,15,20,12,14,22,7,18,19,22,4,14,13,12,10,15,20,5];
+
+function glande(arraydepenes){
+    return Math.max(...arraydepenes)   
+};
+
+console.log(glande(penes));
+
+console.log(Math.max(...penes),"Este es Jordi ENP");
