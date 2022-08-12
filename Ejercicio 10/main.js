b6782b38bdf0e85e9e12966d8546f57b2f70289c
martioc <martioc6464@gmail.com>
2022-08-12 10:20:45 +0200

    vtgfdrgfgf

---
+++ Ejercicio 10/main.js
@@ -0,0 +1,25 @@
+const name = "A MAMARLA PARDA!";
+
+function opa(string){
+  console.log(string.replaceAll("A","o"));
+}
+
+opa(name);
+
+const academia = "academia"
+const escuela = "escuela"
+
+function  ak47 (string){
+  console.log(string.startsWith("aca"))
+};
+
+ak47(academia);
+ak47(escuela);
+
+let saludo = "Hola "
+
+function  salu2 (string){
+  console.log(string.repeat(3))
+};
+
+salu2(saludo);
--- Ejercicio 8/main.js
+++ Ejercicio 8/main.js
@@ -1,18 +1,65 @@
+mes = Math.round(Math.random(1) * 13);
 
-console.log("¿Es cierto que francolachupa?");
-
-let Francolachupa = Math.round(Math.random());
-
-switch (Francolachupa) {
-  case 0:
-    console.log("Si la chupa");
-    break;
+switch (mes) {
   case 1:
-    console.log("Por el culo");
+    console.log(1);
+    break;
+  case 2:
+    console.log(2);
+    break;
+  case 3:
+    console.log(3);
+    break;
+  case 4:
+    console.log(4);
+    break;
+  case 5:
+    console.log(5);
+    break;
+  case 6:
+    console.log(6);
+    break;
+  case 7:
+    console.log(7);
+    break;
+  case 8:
+    console.log(8);
+    break;
+  case 9:
+    console.log(9);
+    break;
+  case 10:
+    console.log(10);
+    break;
+  case 11:
+    console.log(11);
+    break;
+  case 12:
+    console.log(12);
+    break;
+  default:
+    console.log("victor is gay");
     break;
 }
 
-const result = Math.PI.toFixed(3);
-console.log(result);
+Francolachupa = Math.round(Math.random(1) * 6);
 
-console.log(Math.floor(Math.PI * 1000)/1000);
+switch (Francolachupa) {
+  case 1:
+    console.log("One");
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  case 3:
+    console.log("Three");
+    break;
+  case 4:
+    console.log("Four");
+    break;
+  case 5:
+    console.log("Five");
+    default:
+    console.log("Francolachupa");
+    break;
+}
